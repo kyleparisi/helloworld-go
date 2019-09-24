f65e87d86ae7957b872e1834cc6acbf93e3a057c
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-09-24 12:58:17 -0400

    update

--- helloworld.go
+++ helloworld.go
@@ -11,7 +11,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
         log.Print("Hello world received a request.")
         target := os.Getenv("TARGET")
         if target == "" {
-                target = "World!"
+                target = "World! blh blh"
         }
         fmt.Fprintf(w, "Hello %s!\n", target)
 }
